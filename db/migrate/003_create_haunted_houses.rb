ed09316e1b6b29dd302d97bf4f64b9cc0b89e12f
Steven Perfors <stevecanbuild@gmail.com>
2020-04-03 22:11:18 +0000

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
+  def change 
+    create_table :hauntedhouses do |h|
+      h.string :name 
+      h.string :location 
+      h.string :theme 
+      h.float :price 
+      h.boolean :family_friendly
+      h.integer :opening_date 
+      h.integer :closing_date 
+      h.text :description 
+    end
+  end
+end
